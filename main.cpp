8bc95b3b2224377c3c7ea2b1a541e03d7eef468f
astronaut <1017243931@qq.com>
2025-05-21 14:55:10 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,3 +20,21 @@ void* perTest2(char *pCA)
 	BusObj.Exit();
 	return NULL;
 }
+
+void* perTest1(char *pCA)
+{
+	Cgxper BusObj;
+	tagperTest2 BusParam = {0x00};
+
+	DEALTRY{
+		if( !BusObj.Init(pCA) )
+			return NULL;
+
+      BusObj.GetValue( "request", BusParam.sRequest, sizeof(BusParam.sRequest) );
+
+		  BusObj.perTest2(BusParam);
+	}DEALCATCH();
+
+	BusObj.Exit();
+	return NULL;
+}
